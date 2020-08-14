58792adfff363a0a3b79e3b664ef7fe115ee2955
yahoo17 <55846951+yahoo17@users.noreply.github.com>
2020-08-14 18:16:24 +0800

    1

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
-import "fmt"
-import c"k8s-authz/connect"
 
-func main(){
-	fmt.Print("hello ")
-	c.Echo()
+import (
+	"fmt"
+	."k8s-authz/connect"
+	."k8s-authz/itface"
+)
+
+func main() {
+	// fmt.Print("hello ")
+	// Echo()
+	// Decode()
+	// student :=Person{"yanhao",18}
+	// student.Smile()
+	a:=[]int{1,2,2}
+	Do(a)
 }
