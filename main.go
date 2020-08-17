2bedbf9eeab7eacd955cfec63e30ff06882cc471
yahoo17 <yahoo2000@126.com>
2020-08-17 15:17:28 +0800

    1

--- main.go
+++ main.go
@@ -1,70 +1,103 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"io/ioutil"
-	"strings"
+	"io"
+	"log"
+	"net/http"
+ 	"os"
+	"sync"
 )
 
-func main() {
-	counts:=make(map[string]int)
+var  mu sync.Mutex
+var	count int
+func  main()  {
+	http.HandleFunc("/",handler)
+	http.HandleFunc("/count",counter)
 
-	for _, filename :=range os.Args[1:] {
-		data, err := ioutil.ReadFile(filename)
+	log.Fatal(http.ListenAndServe("localhost:8000",nil))
+}
 
-		if err!=nil{
-			fmt.Println("Read file err")
-		}
-		for _,line := range strings.Split(string(data),"\n"){
-			counts[line]++
-		}
-		for key,value :=range counts{
-			if value>1 {
-				fmt.Println(filename)
-				delete(counts, key)
-				break
-			}
-		}
+func handler(w http.ResponseWriter,r *http.Request)  {
 
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w,"URL path access by you is %q", r.URL.Path)
 
-	}
-
+}
+func counter(w http.ResponseWriter,r* http.Request)  {
+	mu.Lock()
+	fmt.Fprintf(w,"現在有%v個人訪問過此頁面",count)
+	mu.Unlock()
 
 }
 
-//
-func ReadFromFile(){
-	counts  :=make(map[string]int)
-	files:=os.Args[1:]
-	if len(files)==0{
-		countLines(os.Stdin,counts)
-	}else{
-		for _,arg:= range files{
-			f,err :=os.Open(arg)
-			if err!=nil{
-				fmt.Println("打开文件错误")
 
-			}
 
-			countLines(f,counts)
-			f.Close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func network(){
+
+
+	for _,url := range os.Args[1:] {
+		if url[0:7]!="http://" {
+			url="http://"+url
+		}
+		res,err:=http.Get(url)
+		b:=res.Status
+		if err!=nil {
+			fmt.Fprintf(os.Stderr,"fetch: %v\n http状态码等于%s",err,b)
+			os.Exit(1)
 		}
 
-		for  line, _:=range counts{
-			fmt.Println(line)
+		io.Copy(os.Stdout,res.Body)
+		res.Body.Close()
+		if  err!=nil{
+			fmt.Fprintf(os.Stderr,"fetch  :reading %s %v",url,err)
+			os.Exit(1)
 		}
 
-
 	}
-
-}
-//函数和包级别的变量和声明, 可以任意顺序声明
-func countLines(f * os.File,counts map[string]int)  {
-	input :=bufio.NewScanner(f)
-	for input.Scan(){
-		counts[input.Text()]++
-	}
-
 }
