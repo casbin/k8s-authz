26d6138a11bc257a811bf0c397ead01884f2ccc7
ashish493 <b218008@iiit-bh.ac.in>
2021-04-28 00:46:34 +0530

    Add webhook server for casbin
    Signed-off-by: ashish493 <b218008@iiit-bh.ac.in>

---
+++ server.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/golang/glog"
+	"k8s.io/api/admission/v1beta1"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/casbin/casbin/v2"
+)
+
+type CasbinServerHandler struct {
+}
+
+func (gs *CasbinServerHandler) serve(w http.ResponseWriter, r *http.Request) {
+	var body []byte
+	if r.Body != nil {
+		if data, err := ioutil.ReadAll(r.Body); err == nil {
+			body = data
+		}
+	}
+	if len(body) == 0 {
+		glog.Error("empty body")
+		http.Error(w, "empty body", http.StatusBadRequest)
+		return
+	}
+	glog.Info("Received request")
+
+	if r.URL.Path != "/validate" {
+		glog.Error("no validate")
+		http.Error(w, "no validate", http.StatusBadRequest)
+		return
+	}
+
+	arRequest := v1beta1.AdmissionReview{}
+	if err := json.Unmarshal(body, &arRequest); err != nil {
+		glog.Error("incorrect body")
+		http.Error(w, "incorrect body", http.StatusBadRequest)
+	}
+
+	raw := arRequest.Request.Object.Raw
+	pod := v1.Pod{}
+	if err := json.Unmarshal(raw, &pod); err != nil {
+		glog.Error("error deserializing pod")
+		return
+	}
+	allowed, err := e.Enforce(request)
+	if (err != nil){
+		panic(err)
+			}
+	if allowed {
+	response := v1beta1.AdmissionReview{
+		Response: &v1beta1.AdmissionResponse{
+			Allowed: true,
+		},
+		}
+	}
+	response := v1beta1.AdmissionReview{
+		Response: &v1beta1.AdmissionResponse{
+		  Allowed: false,
+		  Result: &metav1.Status{
+			Message: " You are not authorized to perform any operations on these pods!",
+		  },
+		},
+	  }
+
+	resp, err := json.Marshal(response)
+	if err != nil {
+		glog.Errorf("Can't encode response: %v", err)
+		http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
+	}
+	glog.Infof("Ready to write reponse ...")
+	if _, err := w.Write(resp); err != nil {
+		glog.Errorf("Can't write response: %v", err)
+		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
+	}
+}
