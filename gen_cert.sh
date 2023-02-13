2e01e9721df20cc694c7e51112c8072c21927d88
鱼竿钓鱼干 <46661603+PokIsemaine@users.noreply.github.com>
2023-02-13 18:05:02 +0800

    fix: README.md and some scripts (#36)

--- gen_cert.sh
+++ gen_cert.sh
@@ -6,10 +6,14 @@ set -o pipefail
 
 openssl genrsa -out certs/ca.key 2048
 
-openssl req -new -x509 -key certs/ca.key -out certs/ca.crt 
+openssl req -new -x509 -key certs/ca.key -out certs/ca.crt
 
 openssl genrsa -out certs/casbin-key.pem 2048
 
-openssl req -new -key certs/casbin-key.pem -subj "/CN=casbin.default.svc" -out casbin.csr 
+openssl req -new -key certs/casbin-key.pem -subj "/CN=casbin.default.svc" -out casbin.csr
 
 openssl x509 -req -in casbin.csr -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/casbin-crt.pem
+
+export CA_BUNDLE=$(cat certs/ca.crt | base64 | tr -d '\n')
+
+cat manifests/ValidatingWebhookConf.yaml.template | envsubst > manifests/ValidatingWebhookConf.yaml
